a57f745e91c9a0bcb6e548cb873090c683ae01cc
socratone <gim2origin@gmail.com>
2020-07-08 10:31:16 +0900

    chantSub에 모달 추가

--- src/components/chantSub.jsx
+++ src/components/chantSub.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
 
 const ChantSub = (props) => {
   const { setHidden, handleClickEllipsis } = props;
+  const [modalIsOpen, setModalOpen] = useState(false);
+
+  const customStyles = {
+    content: {
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const afterOpenModal = () => {};
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
 
   const handleClickV = () => {
+    openModal();
     handleClickEllipsis();
   };
 
@@ -22,6 +46,24 @@ const ChantSub = (props) => {
           <i className="fa fa-times" />
         </span>
       </p>
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        // contentLabel="Chant Select Modal"
+      >
+        {/* <h2 ref={_subtitle => (subtitle = _subtitle)}>Hello</h2> */}
+        <button onClick={closeModal}>close</button>
+        <div>어디에 추가하시겠습니까?</div>
+        <form>
+          <input />
+          <button>tab navigation</button>
+          <button>stays</button>
+          <button>inside</button>
+          <button>the modal</button>
+        </form>
+      </Modal>
     </li>
   );
 };
