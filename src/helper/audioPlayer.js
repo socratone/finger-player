190ae407d06bb6fad5d91e0693d631434dd78862
socratone <gim2origin@gmail.com>
2020-07-22 01:31:32 +0900

    Audio API 적용
     - 페이드 아웃의 노이즈는 해결
     - 문제1: 처음 연주시 저음 어택에 잡음이 생김
     - 문제2: 페이드 아웃을 좀 더 가파르게 해야 저음 캔슬레이션 방지

--- src/helper/audioPlayer.js
+++ src/helper/audioPlayer.js
@@ -1,7 +1,9 @@
+const AudioContext = window.AudioContext || window.webkitAudioContext;
+
 const notes1 = {};
 const notes2 = {};
 
-const MASTER_VOLUME = 0.5;
+const MASTER_VOLUME = 0;
 const INTERVAL = 1;
 
 const convertNotation = (note) => {
@@ -16,8 +18,28 @@ const convertNotation = (note) => {
 const loadNote = (...pitchs) => {
   pitchs.forEach(async (pitch) => {
     pitch = convertNotation(pitch);
-    notes1[pitch] = await new Audio(`../audio/${pitch}.mp3`);
-    notes2[pitch] = await new Audio(`../audio/${pitch}.mp3`);
+    notes1[pitch] = { audio: await new Audio(`../audio/${pitch}.mp3`) };
+    notes2[pitch] = { audio: await new Audio(`../audio/${pitch}.mp3`) };
+
+    const audioContext1 = new AudioContext();
+    const audioContext2 = new AudioContext();
+
+    // pass it into the audio context
+    const track1 = audioContext1.createMediaElementSource(notes1[pitch].audio);
+    const track2 = audioContext2.createMediaElementSource(notes2[pitch].audio);
+    track1.connect(audioContext1.destination);
+    track2.connect(audioContext2.destination);
+
+    // 게인 조절
+    const gainNode1 = audioContext1.createGain();
+    const gainNode2 = audioContext2.createGain();
+    track1.connect(gainNode1).connect(audioContext1.destination);
+    track2.connect(gainNode2).connect(audioContext2.destination);
+
+    notes1[pitch]['gainNode'] = gainNode1;
+    notes2[pitch]['gainNode'] = gainNode2;
+    notes1[pitch]['audioContext'] = audioContext1;
+    notes2[pitch]['audioContext'] = audioContext2;
   });
 };
 
@@ -25,41 +47,37 @@ const playNote = (pitch) => {
   pitch = convertNotation(pitch);
   const note1 = notes1[pitch];
   const note2 = notes2[pitch];
-  if (note1.currentTime === 0) {
-    console.log('note1 재생');
-    note1.volume = MASTER_VOLUME;
-    note1.play();
+  if (note1.audio.currentTime === 0) {
+    if (note1.audioContext.state === 'suspended') note1.audioContext.resume();
+    // console.log('note1 재생');
+    note1.gainNode.gain.value = MASTER_VOLUME;
+    note1.audio.play();
     return note1;
-  } else if (note2.currentTime === 0) {
-    console.log('note2 재생');
-    note2.volume = MASTER_VOLUME;
-    note2.play();
+  } else if (note2.audio.currentTime === 0) {
+    if (note2.audioContext.state === 'suspended') note2.audioContext.resume();
+    // console.log('note2 재생');
+    note2.gainNode.gain.value = MASTER_VOLUME;
+    note2.audio.play();
     return note2;
   }
 };
 
-const stopNote = (player) => {
+const stopNote = (note) => {
   let volume = MASTER_VOLUME;
 
+  // console.log('note:', note);
   const fadeout = setInterval(() => {
     volume -= 0.01;
-    if (volume > 0) {
-      player.volume = volume.toFixed(2);
-      // console.log("볼륨 : ", player.volume);
+    if (volume > -1) {
+      note.gainNode.gain.value = volume.toFixed(2);
     } else {
-      // console.log("끝날 때 : ", player.volume);
       clearInterval(fadeout);
-      player.volume = 0;
-      player.pause();
-      player.currentTime = 0;
+      note.gainNode.gain.value = -1;
+      note.audio.pause();
+      note.audio.currentTime = 0;
       return undefined;
     }
   }, INTERVAL);
-
-  // player.volume = 0;
-  // player.pause();
-  // player.currentTime = 0;
-  // return undefined;
 };
 
 module.exports = { loadNote, playNote, stopNote };
