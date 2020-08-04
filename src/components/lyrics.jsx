fab9725a20b2050cbfde0ba0dc23f1715680bc4b
socratone <gim2origin@gmail.com>
2020-08-04 17:46:06 +0900

    변수 이름 수정

--- src/components/lyrics.jsx
+++ src/components/lyrics.jsx
@@ -4,7 +4,7 @@ import Word from './word';
 const PLAYED_WORD_COLOR = 'green';
 
 const Lyrics = (props) => {
-  const { lyricsNumber, currentLyricsIndex, lyrics, wordIndex } = props;
+  const { lyricsNumber, currentLyricsNumber, lyrics, wordIndex } = props;
   const words = lyrics.split('');
 
   const wordParent = React.createRef();
@@ -12,22 +12,22 @@ const Lyrics = (props) => {
   // 연주되는 가사가 녹색으로 바뀐다.
   useEffect(() => {
     const spans = wordParent.current.children;
-    if (lyricsNumber === currentLyricsIndex && spans[wordIndex]) {
+    if (lyricsNumber === currentLyricsNumber && spans[wordIndex]) {
       spans[wordIndex].style.color = PLAYED_WORD_COLOR;
       spans[wordIndex].style.fontWeight = '900';
     }
-  }, [wordIndex, lyricsNumber, currentLyricsIndex, wordParent]);
+  }, [wordIndex, lyricsNumber, currentLyricsNumber, wordParent]);
 
   // 절이 바뀌면 글자가 초기화 된다.
   useEffect(() => {
     const spans = wordParent.current.children;
-    if (lyricsNumber !== currentLyricsIndex) {
+    if (lyricsNumber !== currentLyricsNumber) {
       for (let i = 0; i < spans.length; i++) {
         spans[i].style.color = 'unset';
         spans[i].style.fontWeight = 'unset';
       }
     }
-  }, [currentLyricsIndex]);
+  }, [currentLyricsNumber]);
 
   if (lyricsNumber === 1) {
     let index = -1;
