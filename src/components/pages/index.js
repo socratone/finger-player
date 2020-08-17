e9a0c5c0a36b9d6e07a96dd09ffeaecdbba2c46a
socratone <gim2origin@gmail.com>
2020-08-18 02:37:17 +0900

    settings 폴더 생성, 파일 이동

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -2,4 +2,4 @@ export { default as Home } from './home/home';
 export { default as Chants } from './chants/chants';
 export { default as Player } from './player/player';
 export { default as Search } from './search/search';
-export { default as Settings } from './settings';
+export { default as Settings } from './settings/settings';
