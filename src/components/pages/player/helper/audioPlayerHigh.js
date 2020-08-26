fe49593f41ef254f1c62c732d022b3eb5f69eedc
socratone <gim2origin@gmail.com>
2020-08-27 01:11:19 +0900

    audioPlayer 성능 개선

--- src/components/pages/player/helper/audioPlayerHigh.js
+++ src/components/pages/player/helper/audioPlayerHigh.js
@@ -1,9 +1,9 @@
-const AudioContext = window.AudioContext || window.webkitAudioContext;
+// const AudioContext = window.AudioContext || window.webkitAudioContext;
+const context = new AudioContext();
 
-let notes1 = {};
-let notes2 = {};
+let notes = {};
 
-// const MASTER_VOLUME = -0.5;
+const VOLUME = 0.5;
 const FADEOUT_SECONDS = 0.2;
 
 const convertNotation = (note) => {
@@ -18,64 +18,39 @@ const convertNotation = (note) => {
 const loadNote = (...pitchs) => {
   pitchs.forEach(async (pitch) => {
     pitch = convertNotation(pitch);
-    notes1[pitch] = { audio: await new Audio(`../audio/${pitch}.mp3`) };
-    notes2[pitch] = { audio: await new Audio(`../audio/${pitch}.mp3`) };
-
-    const context = new AudioContext();
-
-    // pass it into the audio context
-    const source1 = context.createMediaElementSource(notes1[pitch].audio);
-    const source2 = context.createMediaElementSource(notes2[pitch].audio);
-
-    // 게인 조절
-    const gainNode1 = context.createGain();
-    const gainNode2 = context.createGain();
-
-    gainNode1.gain.setValueAtTime(0, context.currentTime);
-    gainNode2.gain.setValueAtTime(0, context.currentTime);
-    source1.connect(gainNode1);
-    source2.connect(gainNode2);
-    gainNode1.connect(context.destination);
-    gainNode2.connect(context.destination);
-
-    notes1[pitch]['gainNode'] = gainNode1;
-    notes2[pitch]['gainNode'] = gainNode2;
-    notes1[pitch]['audioContext'] = context;
-    notes2[pitch]['audioContext'] = context;
+    fetch(`../audio/${pitch}.mp3`)
+      .then((data) => data.arrayBuffer())
+      .then((arrayBuffer) => context.decodeAudioData(arrayBuffer))
+      .then((decodedAudio) => {
+        notes[pitch] = decodedAudio;
+      });
   });
 };
 
 const playNote = (pitch) => {
   pitch = convertNotation(pitch);
-  const note1 = notes1[pitch];
-  const note2 = notes2[pitch];
-  if (note1.audio.currentTime === 0) {
-    if (note1.audioContext.state === 'suspended') note1.audioContext.resume();
-    console.log('note1 재생');
-    note1.gainNode.gain.setValueAtTime(1, note1.audioContext.currentTime);
-    note1.audio.play();
-    return note1;
-  } else if (note2.audio.currentTime === 0) {
-    if (note2.audioContext.state === 'suspended') note2.audioContext.resume();
-    console.log('note2 재생');
-    note2.gainNode.gain.setValueAtTime(1, note2.audioContext.currentTime);
-    note2.audio.play();
-    return note2;
-  }
+  const audio = notes[pitch];
+
+  const source = context.createBufferSource();
+  source.buffer = audio;
+
+  const gainNode = context.createGain();
+  gainNode.gain.setValueAtTime(VOLUME, context.currentTime);
+
+  source.connect(gainNode);
+  gainNode.connect(context.destination);
+  source.start(context.currentTime);
+
+  return { source, gainNode };
 };
 
-const fadeoutNote = (note) => {
-  const { audio, gainNode, audioContext } = note;
-  gainNode.gain.setValueAtTime(gainNode.gain.value, audioContext.currentTime);
-  gainNode.gain.exponentialRampToValueAtTime(
-    0.0001,
-    audioContext.currentTime + FADEOUT_SECONDS
+const fadeoutNote = ({ source, gainNode }) => {
+  gainNode.gain.setValueAtTime(VOLUME, context.currentTime);
+  gainNode.gain.linearRampToValueAtTime(
+    0,
+    context.currentTime + FADEOUT_SECONDS
   );
-
-  setTimeout(() => {
-    audio.pause();
-    audio.currentTime = 0;
-  }, FADEOUT_SECONDS * 1000 + 10);
+  source.stop(context.currentTime + FADEOUT_SECONDS + 0.1);
 };
 
 const removeNote = (...notes) => {
@@ -85,14 +60,13 @@ const removeNote = (...notes) => {
 
   setTimeout(function () {
     console.log('메모리에서 노트 제거');
-    for (let pitch in notes1) {
-      notes1[pitch].audioContext.close();
+    for (let pitch in notes) {
+      notes[pitch].audioContext.close();
     }
     // for (let pitch in notes2) {
     //   notes2[pitch].audioContext.close();
     // }
-    notes1 = {};
-    notes2 = {};
+    notes = {};
   }, FADEOUT_SECONDS * 1000 + 100);
 };
 
