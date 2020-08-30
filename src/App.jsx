9e23244a083fa8637e3f8de06466dcdac58d974e
socratone <gim2origin@gmail.com>
2020-08-30 16:16:46 +0900

    HashRouter로 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 
 import { Home, Player, Search, Chants, Settings } from './components/pages';
 import Nav from './components/nav/nav';
